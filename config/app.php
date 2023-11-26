8a9060d8904e33ef078b4952f1a3910029cf4284
mahfuz380 <almahfuz380@gmail.com>
2023-11-27 01:21:51 +0600

    jwt package installed

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
     ])->toArray(),
 
     /*
@@ -183,6 +184,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-    ])->toArray(),
+        'JwtAuth' => Tymon\JWTAuth\Facades\JwtAuth::class,
+        'JwtAuthFactory' => Tymon\JWTAuth\Facades\JwtAuthFactory::class,
+        ])->toArray(),
 
 ];
