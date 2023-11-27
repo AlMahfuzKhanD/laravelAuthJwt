1868efa64c09d2a3e90f8e6764aea2c9346d422c
mahfuz380 <almahfuz380@gmail.com>
2023-11-28 00:47:48 +0600

    login done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,6 +33,25 @@ class AuthController extends Controller
         ],201);
     }
     public function login(Request $request){
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required|string|min:6'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors(),422);
+        }
+        if(!$token=auth()->attempt($validator->validated())){
+            return response()->json(['error'=>'Unauthorized'],401);
+        }
+        return $this->createNewToken($token);
+    }
 
+    public function createNewToken($token){
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'bearer',
+            'expires_in'=>auth()->factory()->getTTL()*60,
+            'user'=>auth()->user()
+        ]);
     }
 }
